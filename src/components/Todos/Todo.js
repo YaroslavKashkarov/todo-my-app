763e400f49552551d8a54fed0cc8635f2844c97d
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2026-01-14 22:21:12 +0100

    added new propsafor the file Todo.module.css that be changing colors and status icons

--- src/components/Todos/Todo.js
+++ src/components/Todos/Todo.js
@@ -1,10 +1,14 @@
-import {RiTodoFill} from 'react-icons/ri'
+import {RiDeleteBin2Line, RiTodoFill} from 'react-icons/ri'
 import styles from './Todo.module.css'
+import {FaCheck} from 'react-icons/fa'
+// @ts-ignore
 function Todo({todo, deleteTodo}) {
   return (
     <div className={styles.todo} onDoubleClick={() => deleteTodo(todo.id)}>
       <RiTodoFill className={styles.todoIcon} />
       <div className={styles.todoText}>{todo.text}</div>
+      <RiDeleteBin2Line />
+      <FaCheck />
     </div>
   )
 }
